60ae9cda9682a8533d54d5efe721371814f6a1ff
Ed Rutter <ed.rutter@just-eat.com>
2018-04-19 14:24:15 +0100

    Update json formatting

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -5,13 +5,16 @@ extension Droplet {
         get("campaigntimes") { req in
             var json = JSON()
             
-            try json.set("campaigns", [["valentines" : ["start": Date.with(year: 2018, month: 2, day: 14, hour: 0, minute: 0),
-                                                        "end": Date.with(year: 2018, month: 2, day: 15, hour: 0, minute: 0)]],
-                                       ["christmas": ["start": Date.with(year: 2018, month: 12, day: 20, hour: 0, minute: 0),
-                                                      "end": Date.with(year: 2018, month: 12, day: 25, hour: 0, minute: 0)]],
-                                       ["xfactor": ["start": Date.with(year: 2018, month: 9, day: 20, hour: 0, minute: 0),
-                                                    "end": Date.with(year: 2018, month: 12, day: 3, hour: 0, minute: 0),
-                                                    "imageurl": "https://cdn.itv.com/uploads/show/show_branding_logo/64/7PcsLO1kx2-hCm4iLNaRW6ECPyj7El1iL8DE7K4z4w4.png"]]])
+            try json.set("campaigns", [["name" : "valentines",
+                                        "start": Date.with(year: 2018, month: 2, day: 14, hour: 0, minute: 0),
+                                        "end": Date.with(year: 2018, month: 2, day: 15, hour: 0, minute: 0)],
+                                       ["name" : "christmas",
+                                        "start": Date.with(year: 2018, month: 12, day: 20, hour: 0, minute: 0),
+                                        "end": Date.with(year: 2018, month: 12, day: 25, hour: 0, minute: 0)],
+                                       ["name" : "xfactor",
+                                        "start": Date.with(year: 2018, month: 9, day: 20, hour: 0, minute: 0),
+                                        "end": Date.with(year: 2018, month: 12, day: 3, hour: 0, minute: 0),
+                                        "imageurl": "https://cdn.itv.com/uploads/show/show_branding_logo/64/7PcsLO1kx2-hCm4iLNaRW6ECPyj7El1iL8DE7K4z4w4.png"]])
             return json
         }
         
