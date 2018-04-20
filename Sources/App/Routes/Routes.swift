7c2cc78615170f813847a454f2f4d13db7299d60
Ed Rutter <ed.rutter@just-eat.com>
2018-04-20 12:11:31 +0100

    Remove meal times

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -7,16 +7,16 @@ extension Droplet {
         get("campaigntimes") { req in
             var json = JSON()
             
-            let startOfDay = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())
-            
-            let breakfastStart = Calendar.current.date(bySettingHour: 4, minute: 30, second: 0, of: Date())
-            let breakfastEnd = Calendar.current.date(bySettingHour: 11, minute: 30, second: 0, of: Date())
-            
-            let dinnertStart = Calendar.current.date(bySettingHour: 17, minute: 0, second: 0, of: Date())
-            let dinnerEnd = Calendar.current.date(bySettingHour: 11, minute: 00, second: 0, of: Date())
-            
-            let valentinesDayStart = Date.with(year: 2018, month: 2, day: 14, hour: 0, minute: 0)
-            let valentinesDayEnd = Date.with(year: 2018, month: 2, day: 15, hour: 0, minute: 0)
+//            let startOfDay = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())
+//
+//            let breakfastStart = Calendar.current.date(bySettingHour: 4, minute: 30, second: 0, of: Date())
+//            let breakfastEnd = Calendar.current.date(bySettingHour: 11, minute: 30, second: 0, of: Date())
+//
+//            let dinnertStart = Calendar.current.date(bySettingHour: 17, minute: 0, second: 0, of: Date())
+//            let dinnerEnd = Calendar.current.date(bySettingHour: 11, minute: 00, second: 0, of: Date())
+//
+//            let valentinesDayStart = Date.with(year: 2018, month: 2, day: 14, hour: 0, minute: 0)
+//            let valentinesDayEnd = Date.with(year: 2018, month: 2, day: 15, hour: 0, minute: 0)
             
             let christmasStart = Date.with(year: 2018, month: 12, day: 20, hour: 0, minute: 0)
             let christmasEnd = Date.with(year: 2018, month: 12, day: 25, hour: 0, minute: 0)
@@ -28,22 +28,22 @@ extension Droplet {
                                         "start": Date() + TimeInterval(5.0*60.0),
                                         "end": Date() + TimeInterval(6.0*60.0),
                                         "tagline": "🐐🐐🐐🐐🐐🐐🐐"],
-                                       ["name" : "midnight_feast",
-                                        "start": startOfDay,
-                                        "end": breakfastStart,
-                                        "tagline": "Had a few too many? Close one eye for faster ordering"],
-                                       ["name" : "breakfast",
-                                        "start": breakfastStart,
-                                        "end": breakfastEnd,
-                                        "tagline": "Wake up and smell the bacon"],
-                                       ["name" : "dinner",
-                                        "start": dinnertStart,
-                                        "end": dinnerEnd,
-                                        "tagline": "Avoid an argument over who's cooking, Just Eat"],
-                                       ["name" : "valentines_day",
-                                        "start": valentinesDayStart,
-                                        "end": valentinesDayEnd,
-                                        "tagline": "🍕+🌭 = 😍"],
+//                                       ["name" : "midnight_feast",
+//                                        "start": startOfDay,
+//                                        "end": breakfastStart,
+//                                        "tagline": "Had a few too many? Close one eye for faster ordering"],
+//                                       ["name" : "breakfast",
+//                                        "start": breakfastStart,
+//                                        "end": breakfastEnd,
+//                                        "tagline": "Wake up and smell the bacon"],
+//                                       ["name" : "dinner",
+//                                        "start": dinnertStart,
+//                                        "end": dinnerEnd,
+//                                        "tagline": "Avoid an argument over who's cooking, Just Eat"],
+//                                       ["name" : "valentines_day",
+//                                        "start": valentinesDayStart,
+//                                        "end": valentinesDayEnd,
+//                                        "tagline": "🍕+🌭 = 😍"],
                                        ["name" : "christmas",
                                         "start": christmasStart,
                                         "end": christmasEnd,
